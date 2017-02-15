c54bc2dde4ae55b7dd61966795666f82246217f7
mkotov13 <mkotov13@simons-rock.edu>
2017-02-14 23:03:28 -0500

    Ability to detect start and finish rows

--- mzm.py
+++ mzm.py
@@ -13,6 +13,19 @@ def read(filename):
     encoding = [line.strip('\n') for line in f]
     # possibly calculate the number of columns and rows
     COLUMNS, ROWS = len(encoding[0]), len(encoding)
+
+    start_row, finish_row = -1, -1
+    for i in range(ROWS):
+        # possibly move this inside the symbol_2_int func
+        if encoding[i][-1] == 'X':
+            finish_row += i
+        if encoding[i][0] == 'P':
+            start_row += i
+
+
+    print finish_row, start_row
+
+
     level = [[None] * COLUMNS for j in range(ROWS)]
 
     for i in range(1, ROWS-1):
