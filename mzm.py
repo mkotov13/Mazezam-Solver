b9f0df04e84cbfe24fbac08fa8f0f7a23dae0d5c
mkotov13 <mkotov13@simons-rock.edu>
2017-02-14 23:23:24 -0500

    Successfully implemented the read() function

--- mzm.py
+++ mzm.py
@@ -4,39 +4,40 @@ def symbol_2_int(symbol):
     elif symbol in 'LRC':
         return 1
     elif symbol == 'P':
-        return 3
+        return 2
     else:
         return 9
 
 def read(filename):
     f = open(filename, 'rU')
-    encoding = [line.strip('\n') for line in f]
+    encoding = [list(line.strip('\n')) for line in f]
     # possibly calculate the number of columns and rows
     COLUMNS, ROWS = len(encoding[0]), len(encoding)
 
+    # looks for P in the first column and X in the last column
     start_row, finish_row = -1, -1
     for i in range(ROWS):
         # possibly move this inside the symbol_2_int func
         if encoding[i][-1] == 'X':
-            finish_row += i
+            finish_row = i
         if encoding[i][0] == 'P':
-            start_row += i
+            start_row = i
+    # update the position of P
+    encoding[start_row][3] = 'P'
 
-
-    print finish_row, start_row
-
-
-    level = [[None] * COLUMNS for j in range(ROWS)]
+    level = [[None] * (COLUMNS-6) for j in range(ROWS-2)]
 
     for i in range(1, ROWS-1):
-        for j in range(3, COLUMNS-2):
-            level[i][j] = symbol_2_int(encoding[i][j])
+        for j in range(3, COLUMNS-3):
+            level[i-1][j-3] = symbol_2_int(encoding[i][j])
 
-    # nice-looking print func
+    '''nice-looking print func
     for row in level:
         print row
+    '''
+    return level
 
-
-read('level.txt')
-
-#if __name__ = "__main"":
+if __name__ == "__main__":
+    level = read('level.txt')
+    for row in level:
+        print row
