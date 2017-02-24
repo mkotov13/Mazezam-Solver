fa3a895e6ca3d90252f03442d9eecf18fdf6c86b
mkotov13 <mkotov13@simons-rock.edu>
2017-02-23 20:30:06 -0500

    finished the mechanics functions

--- mzm.py
+++ mzm.py
@@ -31,52 +31,119 @@ def read(filename):
         for j in range(3, COLUMNS-3):
             level[i-1][j-3] = symbol_2_int(encoding[i][j])
 
-    '''nice-looking print func
-    for row in level:
-        print row
-    '''
     # TODO: RAISE error if state matrix has any 9's
     return level, finish_row-1
 
 
-def move_left(state):
-    row = state[1]
-    col = state[2]
-    # check whether the P is in the leftmost c0olumn
+# =========================== Mechanics ===============================
+
+def find_player(matrix):
+    # returns player's position for a given state matrix
+    if matrix:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 2:
+                    return row, col
+
+def move_left(matrix):
+    row, col = find_player(matrix)
+    leftmost = matrix[row].index(1)
+    # check whether the P is in the leftmost column
     if col < 1:
         return False
-    else:
-        # check if there's a block to the left
-        if state[row][col-1] == 1:
-            return False
+    # check if there's a block to the left
+    if matrix[row][col-1] == 1:
+        # if so, check if there's room to move to the left
+        if leftmost < 1:
+            return False        # cannot move
         else:
-            state[row][col-1] = 2
-            state[row][col] = 0
-            return state, row, col-1
+            # shift the row one space to the left
+            matrix[row].append(matrix[row].pop(0))
+            return matrix
+    # if no block to the left, move the person there
+    else:
+        matrix[row][col-1] = 2
+        matrix[row][col] = 0
+        return matrix
 
-def move_down(state):
-    row = state[1]
-    col = state[2]
-    matrix = state[0]
-    # check whether the P is in the leftmost c0olumn
-    if row == len(matrix):
+def move_right(matrix):
+    row, col = find_player(matrix)
+    rightmost = matrix[row][::-1].index(1)
+    # check whether the P is in the rightmost column
+    if col == (len(matrix[0])-1):
+        return False
+    # check if there's a block to the right
+    if matrix[row][col+1] == 1:
+        if rightmost < 1:
+            return False        # no room to move to the right
+        else:
+            # shift the row one space to the right
+            matrix[row].insert(matrix[row].pop(), 0)
+            return matrix
+    else:
+        matrix[row][col+1] = 2
+        matrix[row][col] = 0
+        return matrix
+
+def move_down(matrix):
+    row, col = find_player(matrix)
+    # check whether the P is in the bottom row
+    if row == (len(matrix)-1):
+        return False
+    # check if there's a block below
+    if matrix[row+1][col] == 1:
         return False
     else:
-        # check if there's a block to the left
-        if matrix[row-1][col] == 1:
-            return False
-        else:
-            matrix[row-1][col] = 2   # reassign current player position
-            matrix[row][col] = 0     # clear player's previos position
-            return matrix, row, col-1
+        matrix[row+1][col] = 2   # reassign current player position
+        matrix[row][col] = 0     # clear player's previos position
+        return matrix
 
+
+def move_up(matrix):
+    row, col = find_player(matrix)
+    # check whether the P is in the top row
+    if row == 0:
+        return False
+    # check if there's a block below
+    if matrix[row-1][col] == 1:
+        return False
+    else:
+        matrix[row-1][col] = 2   # reassign current player position
+        matrix[row][col] = 0     # clear player's previos position
+        return matrix
+
+
+def print_state(matrix):
+    if matrix:
+        print '\n'
+        for row in matrix:
+            print row
+    else:
+        print False
 
 if __name__ == "__main__":
     # TODO: enquire which level should be read in
-    level = read('levels/level0.txt')
-    for row in level[0]:
-        print row
-    print level[1]
-    print len(level[0])
-    state = (level[0], 0, 0)
-    move_down(state)
+    level, finish_row = read('levels/level0.txt')
+    # make a copy of the initial matrix, store it in the variable level
+    state = level
+    print finish_row
+    print_state(state)
+    print_state(move_down(state))
+    print 'neighbors'
+    print neighbors(state)
+    '''
+    print_state(move_down(state))
+    print_state(move_down(state))
+    print_state(move_up(state))
+    #print_state(move_up(state))
+    print_state(move_down(state))
+    print_state(move_right(state))
+    print_state(move_right(state))
+    print_state(move_up(state))
+    print_state(move_right(state))
+    print_state(move_down(state))
+    #print_state(move_left(state))
+    print_state(move_up(state))
+    print_state(move_left(state))
+    print_state(move_left(state))
+    '''
